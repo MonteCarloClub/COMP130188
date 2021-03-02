ba8be906e5b37c616774c0eb56ae10c1280fe4d6
hillmoonbird <72114950+hillmoonbird@users.noreply.github.com>
2021-03-02 16:28:59 +0800

    文件移动

---
+++ 指导文件/lab1-2.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 17:28:08 2020
+
+@author: Qimin Ma
+"""
+def decrypt(ciphertext, key):
+    ciphertext = ciphertext.split()
+    plaintext = ""
+    """Returns plaintext decrypted by playfair cipher using key"""
+    # WRITE YOUR CODE HERE!
+
+
+
+
+
+    
+    print(plaintext)
+    return plaintext
+        
+     
+def create_matrix_M(key):
+    """Returns matrix created by playfair cipher using key"""
+    # WRITE YOUR CODE HERE!
+    
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    decrypt("EQ VS ZT ES FS GZ","FUDAN")
